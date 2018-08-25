247f43e6489722bc602de4650f9903bf49f0b979
David Engelstad <davidengelstad@gmail.com>
2018-08-25 12:06:12 -0700

    exercise-4

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ Stop your server and comment out your server code in node-server/index.js. In ex
 
 #Exercise-3 <br />
 Inside your express-server folder, create a file called "routes.js" and a file called "controller.js". In your index.js file, make it so that requests coming in to the endpoint "/api" are routed to your express router. Inside your router file, route those requests to your controller. Inside your controller, build functionality to check (console.log) that each endpoint is being hit.
+
+#Exercise-4 <br />
+Build out the functionality of the component such that it can handle GET, POST, and DELETE requests
--- express-server/controller.js
+++ express-server/controller.js
@@ -1,11 +1,24 @@
+const list = {
+  Todos: []
+};
+
 module.exports = {
   fetch: (req, res) => {
-    console.log('In GET...');
+    const { listName } = req.query;
+    if (listName in list) {
+      res.status(200).send(list[listName]);
+    } else {
+      res.status(404).send('List not Found');
+    }
   },
   post: (req, res) => {
-    console.log('In POST...');
+    const { todo, listName } = req.body;
+    list[listName].push(todo);
+    res.status(201).send(list[listName]);
   },
   delete: (req, res) => {
-    console.log('In DELETE...');
+    const { index, listName } = req.query;
+    list[listName].splice(+index, 1);
+    res.status(202).send(list[listName]);
   }
 };
